2d2565f5922dc8f4c3e9874b7f033730f99878ed
Shubham Mohanty <101620532+ShubhamMohanty680@users.noreply.github.com>
2025-07-22 20:43:06 +0530

    Update spotify_api_data_extract.py

--- spotify_api_data_extract.py
+++ spotify_api_data_extract.py
@@ -10,14 +10,14 @@ def lambda_handler(event, context):
     cilent_id = os.environ.get('client_id')
     client_secret = os.environ.get('client_secret')
     
-    client_credentials_manager = SpotifyClientCredentials(client_id=cilent_id, client_secret=client_secret)
-    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-    playlists = sp.user_playlists('spotify')
+    client_credentials_manager=SpotifyClientCredentials(client_id=client_id,client_secret=client_secret)
+    sp=spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    playlists=sp.user_playlists('spotify')
     
-    playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
-    playlist_URI = playlist_link.split("/")[-1].split("?")[0]
+    playlist_link="https://open.spotify.com/playlist/0sDahzOkMWOmLXfTMf2N4N"
+    playlist_URI=playlist_link.split("/")[-1]
     
-    spotify_data = sp.playlist_tracks(playlist_URI)   
+    spotify_data=sp.playlist_tracks(playlist_URI)
     
     cilent = boto3.client('s3')
     
